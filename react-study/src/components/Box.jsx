3f5fadd4c4fde0293df279a18eed2d982aa06279
Seol-hye <hs1507lll@gmail.com>
2026-06-12 16:29:51 +0900

    컴포넌트 활용, props 전달

---
+++ react-study/src/components/Box.jsx
@@ -0,0 +1,39 @@
+
+
+
+// <Box text={arr[0]} detail={detail[0]}/>
+// props = { text:"아침", detail:"졸리다"};
+/*
+function Box(props) {
+    // props : 컴포넌트 내부에 필요로 하는 매개변수 전달받기
+
+    return (
+        <div style={{ backgroundColor: 'lightblue' }}>
+            <h1>{props.text}</h1>
+            <p>{props.detail}</p>
+        </div>
+    )
+
+    // return (
+    //     <div>
+    //         <h2>Box입니다.</h2>
+    //     </div>
+    // )
+
+}
+*/
+
+// props = { text:"아침", detail:"졸리다"};
+// {text, detail} = { text:"아침", detail:"졸리다"}
+function Box( {text, detail} ) {
+    // props : 컴포넌트 내부에 필요로 하는 매개변수 전달받기
+
+    return (
+        <div style={{ backgroundColor: 'lightblue' }}>
+            <h1>{text}</h1>
+            <p>{detail}</p>
+        </div>
+    )
+}
+
+export default Box;
