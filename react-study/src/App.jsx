7741101d0dbad05f67d299cf8a444b0441e28c84
Seol-hye <hs1507lll@gmail.com>
2026-06-15 14:47:20 +0900

    1. NewsBlog 페이지 초기세팅 2. State 변수 기반 Modal 창 표시여부 처리 3. event.stopPropagaion()

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -11,9 +11,12 @@ import Quiz01 from './quiz/quiz01/Quiz01'
 import State02 from './components/State02'
 import State03 from './components/State03'
 import Props01 from './components/Props01'
+
 import Quiz02 from './quiz/quiz02/Quiz02'
 import Props02 from './components/Props02'
 import Quiz03 from './quiz/quiz03/Quiz03'
+
+import NewsBlog from './newsblog/NewsBlog'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +32,8 @@ function App() {
     // <Props01/>
     // <Quiz02/>
     // <Props02/>
-    <Quiz03/>
+    // <Quiz03/>
+    <NewsBlog/>
 
   )
 }
